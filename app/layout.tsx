2ace0fba478008d8171e31785c03a5ffc9b774e5
Chris O'Keefe <chris@restorestl.com>
2026-02-07 12:28:19 -0600

    Add GTM container GTM-M7DX6C4X

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Montserrat, Inter } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import CrispChat from "./components/CrispChat";
 
@@ -56,7 +57,26 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Script
+        id="gtm-script"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-M7DX6C4X');`,
+        }}
+      />
       <body className={`${montserrat.variable} ${inter.variable} antialiased`}>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-M7DX6C4X"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         {children}
         <CrispChat />
       </body>
